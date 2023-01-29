7eb125795cec1d3d8e7f00044ed7285ea7e0ad02
Robert Boyd III <git@robertboyd.dev>
2023-01-29 11:58:52 -0500

    More stuff

---
+++ src/stores/store.ts
@@ -0,0 +1,37 @@
+import { browser } from '$app/environment';
+import { type Readable, writable } from 'svelte/store';
+import type Card from '../components/Card.svelte';
+
+const cardsWritable = writable<Record<string, Card>>(
+  browser ? JSON.parse(localStorage.getItem('card') ?? '{}') : {}
+);
+
+const isSavingWritable = writable(false);
+export const isSaving: Readable<boolean> = {
+  subscribe: isSavingWritable.subscribe,
+};
+
+const removeCard = (uuid: string) => {
+  cardsWritable.update((c) => {
+    delete c[uuid];
+    return c;
+  });
+};
+
+export const cards = {
+  subscribe: cardsWritable.subscribe,
+  set: cardsWritable.set,
+  removeCard,
+};
+
+let oldTimeout: NodeJS.Timeout;
+cards.subscribe((value) => {
+  clearTimeout(oldTimeout);
+  isSavingWritable.set(true);
+  oldTimeout = setTimeout(() => {
+    if (browser) {
+      localStorage.setItem('card', JSON.stringify(value));
+    }
+    isSavingWritable.set(false);
+  }, 1000);
+});
